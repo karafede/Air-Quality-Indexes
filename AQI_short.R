bb844e64cc1dfa8ff210cc03c51f14442151b682
karafede <karafede@hotmail.com>
2017-09-25 15:15:54 +0400

    Add files via upload

--- AQI_short.R
+++ AQI_short.R
@@ -553,9 +553,60 @@ AQ_data_AQI_Pollutant <- AQ_data_AQI_Pollutant %>%
   filter(Date >= "2015-03-29" & Date <= "2015-04-04") 
 
 write.csv(AQ_data_AQI_Pollutant, "D:/AQI/AQI_all_DUST_event_2015.csv")
+
+#################################################################################
+########## reload data ##########################################################
+
+library(dplyr)
+library(leaflet)
+library(readr)
+library(lubridate)
+library(ggplot2)
+
+
 AQ_data_AQI_Pollutant <- read_csv("D:/AQI/AQI_all_DUST_event_2015.csv")
 
 str(AQ_data_AQI_Pollutant)
+
+
+AQ_data_AQI_max <- AQ_data_AQI_Pollutant %>%
+  group_by(Site,
+           Date,
+           Hour,
+           max_Pollutant) %>%
+  summarize(max_AQI = max(max_AQI))
+
+
+AQ_data_AQI_Pollutant <- AQ_data_AQI_Pollutant %>%
+  group_by(Site,
+           Date,
+           Hour) %>%
+  summarize(max_AQI = max(max_AQI))
+
+
+AQ_data_AQI_Pollutant <- AQ_data_AQI_max %>%
+  left_join(AQ_data_AQI_Pollutant, by = c("Site", "Date", "Hour", "max_AQI"))
+
+# remove lines wtih NA in the max_AQI column
+AQ_data_AQI_Pollutant <- AQ_data_AQI_Pollutant[!is.na(AQ_data_AQI_Pollutant$max_AQI),]
+
+AQ_data_AQI_Pollutant <- AQ_data_AQI_Pollutant %>%
+  select(Site,
+         Date,
+         Hour,
+         max_AQI,
+         max_Pollutant)
+
+
+
+# create a time field
+AQ_data_AQI_Pollutant$DATETIME <- paste0(AQ_data_AQI_Pollutant$Date, " ", AQ_data_AQI_Pollutant$Hour, ":00")
+
+AQ_data_AQI_Pollutant <- AQ_data_AQI_Pollutant %>%
+  mutate(DateTime = ymd_hm(DATETIME))
+
+AQ_data_AQI_Pollutant <- na.omit(AQ_data_AQI_Pollutant)
+
 
 #########################################################
 # make plots for all the monitoring stations (facets) ###
@@ -585,17 +636,6 @@ plot
 
 par(oldpar)
 dev.off()
-
-######################################################################################
-##### calculate average AQI at national level during the days of the dust event #####
-
-UAE_AQI_DUST <- AQ_data_AQI_Pollutant %>%
-  group_by(DateTime) %>%
-  summarise(mean_AQI = mean(max_AQI))
-
-
-min <- as.POSIXct("2015-03-29 01:00:00") 
-max <- as.POSIXct("2015-04-05 13:00:00") 
 
 
 ############################################################################
@@ -651,6 +691,57 @@ plot
 par(oldpar)
 dev.off()
 
+
+
+################################################################################
+##### calculate average AQI at national level during the days of the dust event #####
+
+UAE_AQI_DUST <- AQ_data_AQI_Pollutant %>%
+  group_by(DateTime) %>%
+  summarise(mean_AQI = mean(max_AQI))
+
+
+min <- as.POSIXct("2015-03-29 01:00:00") 
+max <- as.POSIXct("2015-04-05 13:00:00") 
+
+
+jpeg('D:/AQI/UAE_MEAN_AQI_DUST_EVENT_2015_hours.jpg',
+     quality = 100, bg = "white", res = 300, width = 18, height = 9, units = "in")
+par(mar=c(4, 10, 9, 2) + 0.3)
+oldpar <- par(las=1)
+
+
+plot <- ggplot(UAE_AQI_DUST, aes(DateTime, mean_AQI)) + 
+  theme_bw() +
+  geom_line(aes(y = mean_AQI, col = "mean_AQI"), alpha=1, col="black", size =2) +
+  theme(legend.position="none") + 
+  ylab(expression(paste("mean AQI"))) +
+  theme(axis.title.x=element_blank(),
+        axis.text.x  = element_text(angle=0, vjust=0.5, hjust = 0.5, size=30, colour = "black", face="bold")) +
+  theme(axis.title.y = element_text(face="bold", colour="black", size=30),
+        axis.text.y  = element_text(angle=0, vjust=0.5, size=30, colour = "black")) +
+  #  ylim(0, 170)  +
+  xlim(min, max) +
+  
+  # add line for the AQI level and the Category
+  geom_hline(yintercept =50, col="#00CD00", lty=1, size = 2) +
+  #  geom_text(aes(x = min  , y = 55, label = "Good"), size = 6) +
+  
+  
+  geom_hline(yintercept = 100, col="#ffff00", lty=1, size=2) +
+  #  geom_text(aes(x = min , y = 105, label = "Moderate"), size = 6) +
+  
+  geom_hline(yintercept = 150, col="#e59400", lty=1, size=2) +
+  # geom_text(aes(x = as.POSIXct("2015-03-30 15:00:00") , y = 155, label = "Unhealthy for Sensitive Groups"), size = 6) 
+  
+  geom_hline(yintercept = 200, col="#ff0000", lty=1, size=2) 
+#  geom_text(aes(x = 0.7 , y = 220, label = "Unhealthy"), size = 3) +
+
+plot
+
+
+par(oldpar)
+dev.off()
 
 
 
